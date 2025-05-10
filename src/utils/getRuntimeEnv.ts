031f0832f876a6f1e95d2f52c285de0cddd233f9
Joan Lluc Fernandez Falco <joan.lluc.fernandez@estudiantat.upc.edu>
2025-05-10 15:59:21 +0200

    injection_v2

--- src/utils/getRuntimeEnv.ts
+++ src/utils/getRuntimeEnv.ts
@@ -5,7 +5,7 @@ export const getRuntimeEnv = (): RuntimeEnv => {
 
   // Optional fallback or throw
   return {
-    NEXT_PUBLIC_API_URL: 'http://localhost:8080',
-    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: '',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN || '',
   };
 };
